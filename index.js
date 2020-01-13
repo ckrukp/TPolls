3d9589921737eb16c42d1b134fa7a610074d6a2c
Devin W. Leaman <dleaman212@gmail.com>
2020-01-13 06:35:49 -0600

    Added `useUnifiedTopology` option to mongoose for deprecation warning

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.pre(versioning({ prefix: '/api' }))
 
 server.listen(SERVER_PORT, () => {
   mongoose.Promise = global.Promise
-  mongoose.connect(config.db.uri, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
+  mongoose.connect(config.db.uri, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
 
   const db = mongoose.connection
 
