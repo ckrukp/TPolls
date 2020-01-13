50fbe69ddc8431839f740346c76bfe9ea239f180
Devin W. Leaman <dleaman212@gmail.com>
2020-01-13 08:01:16 -0600

    Added a getLogger function to `tools.js`

--- src/util/tools.js
+++ src/util/tools.js
@@ -2,4 +2,17 @@ const checkContent = req => {
   return req.is('application/json')
 }
 
+const getLogger = () => {
+  const SNL = require('simple-node-logger')
+  const manager = new SNL()
+  manager.createConsoleAppender()
+  manager.createRollingFileAppender({
+    logDirectory: './logs',
+    fileNamePattern: 'tpolls-<DATE>.log',
+    dateFormat: 'YYYY.MM.DD'
+  })
+  return manager.createLogger()
+}
+
+module.exports.getLogger = getLogger
 module.exports.checkContent = checkContent
